ea984a56152428ffdfe78b5034d4777523efa9c5
Dmitry Komanov <dmitryk@wix.com>
2016-07-27 13:41:23 +0300

    Extract Host

--- mysql-streaming-core/src/main/scala/com/komanov/mysql/streaming/Drivers.scala
+++ mysql-streaming-core/src/main/scala/com/komanov/mysql/streaming/Drivers.scala
@@ -4,6 +4,7 @@ import com.komanov.mysql.streaming.Drivers._
 
 object Drivers {
 
+  val Host = "localhost"
   val Port = 3316
   val UserName = "sa"
   val Password = "sa"
@@ -26,17 +27,17 @@ sealed trait MysqlDriver {
 case object ConnectorJDriver extends MysqlDriver {
   override val name = "ConnectorJ"
 
-  override val url = s"jdbc:mysql://localhost:$Port/$DataBase?user=$UserName&password=$Password&cacheServerConfiguration=true&createDatabaseIfNotExist=false"
+  override val url = s"jdbc:mysql://$Host:$Port/$DataBase?user=$UserName&password=$Password&cacheServerConfiguration=true&createDatabaseIfNotExist=false"
 }
 
 case object MariaDbDriver extends MysqlDriver {
   override val name = "MariaDB"
 
-  override val url = s"jdbc:mariadb://localhost:$Port/$DataBase?user=$UserName&password=$Password&fastConnect=true&tcpAbortiveClose=true"
+  override val url = s"jdbc:mariadb://$Host:$Port/$DataBase?user=$UserName&password=$Password&fastConnect=true&tcpAbortiveClose=true"
 }
 
 case object DrizzleDriver extends MysqlDriver {
   override val name = "Drizzle"
 
-  override val url = s"jdbc:drizzle://$UserName:$Password@localhost:$Port/$DataBase"
+  override val url = s"jdbc:drizzle://$UserName:$Password@$Host:$Port/$DataBase"
 }
