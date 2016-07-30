c6bf2d22e5731055f6842836485f5f40570fff63
Dmitry Komanov <dmitryk@wix.com>
2016-07-30 19:54:47 +0300

    Recreate jmh-results.json file if exists

--- jmh-run.sh
+++ jmh-run.sh
@@ -15,7 +15,7 @@ ec mvn clean install
 ec java -jar mysql-streaming-test-connectorj/target/benchmarks.jar -rf json -rff jmh-connectorj-result.json > jmh-connectorj.log
 ec java -jar mysql-streaming-test-mariadb/target/benchmarks.jar -rf json -rff jmh-mariadb-result.json > jmh-mariadb.log
 
-echo '[' >> jmh-results.json
+echo '[' > jmh-results.json
 cat jmh-connectorj-result.json >> jmh-results.json
 echo ',' >> jmh-results.json
 cat jmh-mariadb-result.json >> jmh-results.json
